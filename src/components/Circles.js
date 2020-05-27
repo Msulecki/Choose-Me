03dfc3573bb0042bbb6cbbaeea3f3795136b4574
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-05-27 20:35:52 +0200

    added transparent border-left for circle animation

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -28,12 +28,12 @@ function Circles(props) {
                 {i + 1}
                 <div
                     style={{
-                        borderColor: `hsla(${100 + (30 * (i + 1))},100%,60%,.7)`
+                        borderColor: `hsla(${100 + (30 * (i + 1))},100%,60%,.7)`,
+                        borderLeft: '10px solid transparent'
                     }}
                     className="app__circle-border"></div>
             </div>)}
             < div className="app__count" > {playerChoosed ? `Player ${winnerId + 1} won.` : circles.length}</div >
-            {/* <div className="app__circle debug">6</div> */}
         </>
     );
 }
