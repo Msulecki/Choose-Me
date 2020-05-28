67111eff57917006e65c8811c37e4de7d02026ff
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-05-28 20:05:29 +0200

    component for message on desktops

---
+++ src/components/Desktop.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Desktop() {
+    return (
+        <div className="app__desktop">It appears that you use desktop version of the browser, without touch functionality. This page works only on mobile with multitouch.</div>
+    );
+}
+export default Desktop;
