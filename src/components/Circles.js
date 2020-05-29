99ee99040fdddf0bef9233ad15eb00d0e66a8985
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-05-29 22:19:21 +0200

    moved logic to inner component

--- src/components/Circles.js
+++ src/components/Circles.js
@@ -1,10 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import "../styles/Circles.scss";
 
 function Circles(props) {
-    const { circles } = props;
+    const [circles, setCircles] = useState([]);
     const [playerChoosed, setPlayerChoosed] = useState(false);
     const [winnerCircle, setWinnerCircle] = useState(0);
     const [winnerId, setWinnerId] = useState(0);
+
+    const handleCirclePosition = e => {
+        const touchesCount = e.targetTouches.length;
+        const positions = [];
+        for (let i = 0; i < touchesCount; i++) {
+            positions.push(e.targetTouches[i])
+        }
+        setCircles(positions);
+    }
 
     const winnerNode = playerChoosed && <div
         style={{
@@ -13,19 +23,21 @@ function Circles(props) {
             backgroundImage: `radial-gradient(hsla(${100 + (30 * (winnerId + 1))},100%,60%,.7), hsla(${110 + (30 * (winnerId + 1))},100%,60%,.7))`
         }
         }
-        className="app__circle winner" >
-        <div className="app__circle-number">{winnerId + 1}</div>
+        className="circle winner" >
+        <div className="circle__number">{winnerId + 1}</div>
         <div
             style={{
                 borderColor: `hsla(${100 + (30 * (winnerId + 1))},100%,60%,.7)`
             }}
-            className="app__circle-border winner">
+            className="circle__border winner">
         </div>
     </div >
 
     useEffect(() => {
         if (circles.length > 1) {
-            const countdown = setTimeout(() => setPlayerChoosed(true), 3000);
+            const countdown = setTimeout(() => {
+                setPlayerChoosed(true);
+            }, 3000);
             return () => clearTimeout(countdown);
         } else if (circles.length === 0) {
             const countdown = setTimeout(() => {
@@ -44,26 +56,29 @@ function Circles(props) {
         }
     }, [circles, playerChoosed, winnerCircle])
     return (
-        <>
+        <div className="circles-field"
+            onTouchStart={handleCirclePosition}
+            onTouchEnd={handleCirclePosition}
+            onTouchMove={handleCirclePosition}>
             {!playerChoosed && circles.map((circle, i) => <div
                 style={{
                     top: circle.clientY,
                     left: circle.clientX,
                     backgroundImage: `radial-gradient(hsla(${100 + (30 * (i + 1))},100%,60%,.7), hsla(${120 + (30 * (i + 1))},100%,60%,.7))`
                 }}
-                className="app__circle"
+                className="circle"
                 key={i}>
-                <div className="app__circle-number">{i + 1}</div>
+                <div className="circle__number">{i + 1}</div>
                 <div
                     style={{
                         borderColor: `hsla(${100 + (30 * (i + 1))},100%,60%,.7)`,
-                        borderLeft: '10px solid transparent'
+                        borderLeft: "10px solid transparent"
                     }}
-                    className="app__circle-border"></div>
+                    className="circle__border"></div>
             </div>)}
             {winnerNode}
             < div className="app__count" > {playerChoosed ? `Player ${winnerId + 1} won.` : circles.length}</div >
-        </>
+        </div>
     );
 }
 export default Circles;
