83edd19ddd13a6530c46c7ff9e5c98f4ff1a7fd4
Mateusz Sulecki <mat.sulecki@gmail.com>
2020-05-31 19:15:46 +0200

    minor styling

--- src/components/WinnerCircle.js
+++ src/components/WinnerCircle.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 function WinnerCircle(props) {
+
     const { winnerCircle, winnerId } = props;
+
     return (
         <div
             style={{
                 top: winnerCircle.clientY,
                 left: winnerCircle.clientX,
                 backgroundImage: `radial-gradient(hsla(${100 + (30 * (winnerId + 1))},100%,60%,.7), hsla(${110 + (30 * (winnerId + 1))},100%,60%,.7))`
-            }
-            }
+            }}
             className="circle winner" >
             <div className="circle__number">{winnerId + 1}</div>
             <div
